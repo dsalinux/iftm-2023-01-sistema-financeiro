4499c393e503f566a345dab5a04418dff417072d
Danilo Souza Almeida <dsalinux@gmail.com>
2023-05-28 18:30:52 -0300

    Melhorias No cadastro de Usuários, inclusão de criptografia.

--- src/main/java/br/edu/iftm/jsf/dao/UsuarioDAO.java
+++ src/main/java/br/edu/iftm/jsf/dao/UsuarioDAO.java
@@ -1,6 +1,7 @@
 package br.edu.iftm.jsf.dao;
 
 import br.edu.iftm.jsf.entity.Usuario;
+import java.util.List;
 
 public class UsuarioDAO extends GenericDAO<Usuario, Long>{
 
@@ -8,6 +9,17 @@ public class UsuarioDAO extends GenericDAO<Usuario, Long>{
     public Class<Usuario> getClassEntity() {
         return Usuario.class;
     }
+
+    @Override
+    public List<Usuario> listar() {
+        List<Usuario> usuarios = super.listar();
+        for (Usuario usuario : usuarios) {
+            usuario.getPermissaoList().size();
+        }
+        return usuarios;
+    }
+    
+    
   
     
 }
